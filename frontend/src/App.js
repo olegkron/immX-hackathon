bd088986bebf9cd5bae0ae2dba4791afa34b6f87
Shtolle <eva.semenov1@gmail.com>
2023-02-11 16:28:32 +0000

    startin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Text } from "@mantine/core";
+import colors from "./constants/colors";
+// import { makeStyles } from "@material-ui/core/styles";
 
 function App() {
+  // const useStyles = makeStyles({
+  //   whiteButton: {
+  //     backgroundColor: "white",
+  //     color: "black",
+  //     "&:hover": {
+  //       backgroundColor: "#E0E0E0",
+  //     },
+  //   },
+  // });
+  // const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: colors.main }}>
+      <div style={{ flexDirection: "row", display: "flex" }}>
+        <Button variant="outline" style={{ color: "white", borderColor: "white" }} color="#000" radius="md" uppercase>
+          Create competition
+        </Button>
+        <Text>PROFILE</Text>
+        <Text>SUBMISSIONS</Text>
+        <Text>HISTORY</Text>
+      </div>
     </div>
   );
 }
