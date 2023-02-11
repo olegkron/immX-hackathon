247aa8035068f44dbf414cab22a7ff1a23095fbc
Oleg Kirillovich <olegkron@ya.ru>
2023-02-11 16:52:32 +0000

    refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,10 @@
-import { Button } from "@mantine/core";
 import "./App.css";
+import AppHeader from "./components/AppHeader";
 import AppScreen from "./components/AppScreen";
-import colors from "./constants/colors";
-
 function App() {
   return (
     <AppScreen>
-      <div className="App" style={{ backgroundColor: colors.main }}>
-        <div style={{ flexDirection: "row", display: "flex", justifyContent: "space-between", paddingHorizontal: 25, alignItems: "center" }}>
-          <div style={{ flexDirection: "row", display: "flex", alignItems: "center" }}>
-            <Button
-              variant="outline"
-              style={{ color: "white", borderColor: "white", display: "flex", marginRight: 15 }}
-              color="#000"
-              radius="md"
-              uppercase
-            >
-              Create competition
-            </Button>
-            <p style={{ margin: 30 }}>PROFILE</p>
-            <p style={{ margin: 30 }}>SUBMISSIONS</p>
-            <p style={{ margin: 30 }}>HISTORY</p>
-          </div>
-          <div style={{ flexDirection: "row", display: "flex" }}>
-            <Button variant="outline" style={{ color: "white", borderColor: colors.mainGradient }} color="#000" radius="md" uppercase>
-              <div style={{ width: 10, height: 10, backgroundColor: "red" }}></div>
-              Connect wallet
-            </Button>
-          </div>
-        </div>
-      </div>
+      <AppHeader />
     </AppScreen>
   );
 }
--- frontend/src/components/AppHeader.js
+++ frontend/src/components/AppHeader.js
@@ -1,18 +1,26 @@
 //header with button "Create competition" on the left, and "Connect wallet" on the right.
 
-import { Button, Header, Text } from "@mantine/core";
+import { Button, Header } from "@mantine/core";
 import React from "react";
+import colors from "../constants/colors";
 
 function AppHeader() {
   return (
     <Header shadow="sm" style={{ backgroundColor: "white" }} radius="md" padding="md">
-      <div style={{ flexDirection: "row", display: "flex" }}>
-        <Button variant="outline" style={{ color: "white", borderColor: "white" }} color="#000" radius="md" uppercase>
-          Create competition
-        </Button>
-        <Text>PROFILE</Text>
-        <Text>SUBMISSIONS</Text>
-        <Text>HISTORY</Text>
+      <div style={{ flexDirection: "row", display: "flex", justifyContent: "space-between", paddingHorizontal: 25, alignItems: "center" }}>
+        <div style={{ flexDirection: "row", display: "flex", alignItems: "center" }}>
+          <Button variant="outline" style={{ color: "white", borderColor: "white", display: "flex", marginRight: 15 }} color="#000" radius="md" uppercase>
+            Create competition
+          </Button>
+          <p style={{ marginRight: 15 }}>PROFILE</p>
+          <p style={{ marginRight: 15 }}>SUBMISSIONS</p>
+          <p style={{ marginRight: 15 }}>HISTORY</p>
+        </div>
+        <div style={{ flexDirection: "row", display: "flex" }}>
+          <Button variant="outline" style={{ color: "white", borderColor: colors.mainGradient }} color="#000" radius="md" uppercase>
+            Create competition
+          </Button>
+        </div>
       </div>
     </Header>
   );
